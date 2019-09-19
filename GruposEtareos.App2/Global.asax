66143af53b07748090983f29949d71470f9a9707
jreyes <jreyes@DESARROLLO9>
2019-09-19 16:57:58 -0500

    Ajustes dll y cambio de versión

---
+++ GruposEtareos.App2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GruposEtareos.App2.MvcApplication" Language="C#" %>
--- GruposEtareos.BI/GruposEtareosModel.Designer.cs
+++ GruposEtareos.BI/GruposEtareosModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'E:\JoseDeLosReyes\RQs\Proyectos Visual\IntegraServicios\GruposEtareos.BI\GruposEtareosModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'E:\JoseDeLosReyes\OtrosProyectos\Pruebas_GruposEtareos\slnPruebasGruposEtareos\GruposEtareos.BI\GruposEtareosModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
